2ddd1e97d181c165d8f6b2560fe3058aabaa7cd5
Molton321 <molton334@gmail.com>
2024-03-26 12:59:56 -0500

    modificacion infromacion proyecto

--- ms-funeraria/app/Controllers/Http/ServiciosController.ts
+++ ms-funeraria/app/Controllers/Http/ServiciosController.ts
@@ -1,40 +1,39 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Servicio from 'App/Models/Servicio';
+import Servicio from 'App/Models/Servicio'
 
 export default class ServiciosController {
-    
-    public async find({request,params}:HttpContextContract){
-        if(params.id){
-            return Servicio.findOrFail(params.id);
-        }else{
-            const data = request.all()
-            if("page" in data && "per_page" in data){
-                const page = request.input('page', 1);
-                const perPage = request.input("per_page",20);
-                return await Servicio.query().paginate(page, perPage)
-            }else{
-                return await Servicio.query()
-            }            
-        }
+  public async find({ request, params }: HttpContextContract) {
+    if (params.id) {
+      return Servicio.findOrFail(params.id)
+    } else {
+      const data = request.all()
+      if ('page' in data && 'per_page' in data) {
+        const page = request.input('page', 1)
+        const perPage = request.input('per_page', 20)
+        return await Servicio.query().paginate(page, perPage)
+      } else {
+        return await Servicio.query()
+      }
     }
+  }
 
-    public async create({request}:HttpContextContract){
-        const body = request.body();
-        const theServicio:Servicio = await Servicio.create(body);
-        return theServicio;
-    }
+  public async create({ request }: HttpContextContract) {
+    const body = request.body()
+    const theServicio: Servicio = await Servicio.create(body)
+    return theServicio
+  }
 
-    public async update({params,request}:HttpContextContract) {
-        const theServicio:Servicio = await Servicio.findOrFail(params.id);
-        const body = request.body();
-        theServicio.fecha = body.fecha;
-        theServicio.estado = body.estado;
-        return theServicio.save();
-    }
+  public async update({ params, request }: HttpContextContract) {
+    const theServicio: Servicio = await Servicio.findOrFail(params.id)
+    const body = request.body()
+    theServicio.fecha = body.fecha
+    theServicio.estado = body.estado
+    return theServicio.save()
+  }
 
-    public async delete({params,response}:HttpContextContract) {
-        const theServicio:Servicio = await Servicio.findOrFail(params.id);
-            response.status(204);
-            return theServicio.delete();
-    }
+  public async delete({ params, response }: HttpContextContract) {
+    const theServicio: Servicio = await Servicio.findOrFail(params.id)
+    response.status(204)
+    return theServicio.delete()
+  }
 }
--- readme.md
+++ readme.md
@@ -1,45 +1,31 @@
 
 # Proyecto FunerariaDigital
 
-El proyecto FunerariaDigital, es un proyecto desarrollado por estudiantes de la clase N° 3196 de la universidad de caldas, en acompañamiento del profesor Felipe Buitrago Carmona, y como objetivo principal afianzar y aplicar conocimientos presentados en la clase de programación III.
+El proyecto FunerariaDigital, es un proyecto desarrollado por estudiantes de la universidad de caldas, en acompañamiento del profesor Felipe Buitrago Carmona, y como objetivo principal afianzar y aplicar conocimientos presentados en la clase de programación III.
 
 ## Requisitos
 
+- Intalar JDK 17 
 - Instalar Node (versión estable): https://nodejs.org/es/ 
 - Instalar MySql: https://dev.mysql.com/downloads/workbench/ 
 - https://www.oracle.com/java/technologies/javase/jdk17-archive-downloads.html
 - https://www.mongodb.com/es/atlas
 
-## Instalación
+## Instalación 
 
 1. Clona este repositorio:
     ``` shell
-    
+    git clone https://github.com/CarlosAlape39671/FunerariaDigital
     ```
-2. Navega hasta el directorio del proyecto.
-3. Crea un entorno virtual para el proyecto:
-    ``` shell
-    
-    ```
-4. Activa el entorno virtual:
-
-- En macOS/Linux:
-    ``` shell
-    
-    ```
-- En Windows (cmd):
-    ``` 
-    
-    ```
-- En Windows (PowerShell):
-    ``` shell
-    
-    ```
-5. Instala las dependencias del proyecto:
+2. Navega hasta el directorio del proyecto Negocio.
     ```shell
-    
+    cd ./ms-funeraria
     ```
-6. Ejecuta el proyecto:
+3. Instala las dependencias del proyecto:
+    ```javascript
+    npm install 
+    ```
+4. Ejecuta el proyecto:
     ``` shell
-    
+    npm run dev
     ```
